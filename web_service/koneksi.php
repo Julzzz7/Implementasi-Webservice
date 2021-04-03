505cf7e49ae25447ea8e8e48e5aa11fed6317f50
Julzzz7 <80964222+Julzzz7@users.noreply.github.com>
2021-04-03 15:58:47 +0700

    Initial commit

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id16417339_resto');
+ define('PASS','b&*jntWEiyAv2Q5)');
+ define('DB','id16417339_restoran');
+
+ $con = new mysqli(HOST,USER,PASS,DB) or die('Unable to Connect');
